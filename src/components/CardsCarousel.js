2bf590e14958de428dc4c1cb220712d13d5ca299
Bruno <brunocmoraes97@gmail.com>
2021-07-17 10:51:34 -0300

    Adiciona title ao carrossel e ajusta a ordem das funcoes de transicao, pois estava causando uma estranheza por parecer um efeito contrario.

--- src/components/CardsCarousel.js
+++ src/components/CardsCarousel.js
@@ -10,16 +10,32 @@ const Container = styled.div`
     width: 328px;
     height: 404px;
   }  
+
+  button {
+    height: 50px;
+    align-self: center;
+    background: #282A35;
+    border: none;
+    color: #FFFFFF;
+    font-size: 25px;
+    text-decoration: none;
+    border-radius: 12px;
+  }
 `
 const Cards = styled.div`
   width: 328px;
   height: 404px;
-  background-color: green;
   margin: 25px;
 
   :nth-of-type(2) {
     margin-top: -50px;
   }
+`
+const TitleCarousel = styled.h1`
+  position: absolute;
+  margin: -15% 22%;
+  width: 60%;
+  font-size: 32px;
 `
 
 function CardsCarousel() {
@@ -37,17 +53,36 @@ function CardsCarousel() {
 
     const indexNext = cardList.indexOf(nextCard.image) + 1;
     setNextCard(data[indexNext]);
-    setLastCard(data[indexNext -1])
-    console.log(indexNext)
+    setLastCard(data[indexNext -1]);
     if (indexNext === lastPosition) setNextCard(data[0]);
+  }
+
+  function lastProject() {
+    const cardList = data.map((item) => item.image);
+
+    const indexActual = cardList.indexOf(actualCard.image) - 1; 
+    setActualCard(data[indexActual]);
+    if (indexActual === -1) setActualCard(data[lastPosition -1]);
+
+    const indexNext = cardList.indexOf(nextCard.image) - 1;
+    setNextCard(data[indexNext]);
+    if (indexNext === -1) setNextCard(data[lastPosition - 1]);
+
+    const indexLast = cardList.indexOf(lastCard.image) -1;
+    setLastCard(data[indexLast]);
+    if (indexLast === -1) setLastCard(data[lastPosition -1]);
 
   }
 
   return (
     <Container>
-      <button type="button" onClick={ nextProject }> proximo </button>
+      <TitleCarousel>Conheça alguns projetos no carrossel abaixo</TitleCarousel>
+      <button type="button" onClick={ nextProject }>Anterior</button>
       <Cards>
-        <img src={ actualCard.image } alt={ actualCard.image } />
+        <img 
+          src={ actualCard.image }
+          alt={ actualCard.image }
+          onClick={ nextProject }/>
         <p>{actualCard.nome}</p>
       </Cards>
       <Cards>
@@ -55,9 +90,14 @@ function CardsCarousel() {
         <p>{ nextCard.nome }</p>
       </Cards>
       <Cards>
-        <img src={ lastCard.image } alt={ lastCard.image } />
+        <img
+          src={ lastCard.image }
+          alt={ lastCard.image }
+          onClick={ lastProject }
+        />
         <p>{ lastCard.nome }</p>
       </Cards>
+      <button type="button" onClick={ lastProject }>Próximo</button>
     </Container>
   )
 }
--- src/components/ContainerProjects.js
+++ src/components/ContainerProjects.js
@@ -5,8 +5,7 @@ import CardsCarousel from './CardsCarousel';
 
 const Container = styled.div`
   width: 1366px;
-  height: 800px;
-  background-color: pink;
+  height: 1000px;
   display: flex;
   justify-content: space-around;
   align-items: center;
